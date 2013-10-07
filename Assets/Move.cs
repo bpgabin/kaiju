e37cb0b335a3ba9927754eb1e9bc93b480297fd0
Brian Gabin <bpgabin@gmail.com>
2013-10-06 18:05:35 -0700

    Initial Project Setup

---
+++ Assets/Move.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+	public float speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = transform.position;
+		newPosition.x += Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+		transform.position = newPosition;
+	}
+}
