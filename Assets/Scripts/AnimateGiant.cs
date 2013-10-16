d136b78e892a86e341231c2bd822bf7f040c3f95
Brian Gabin <bpgabin@gmail.com>
2013-10-16 16:42:36 -0700

    Multiple Audio Sources

--- Assets/Scripts/AnimateGiant.cs
+++ Assets/Scripts/AnimateGiant.cs
@@ -2,13 +2,19 @@
 using System.Collections;
 
 public class AnimateGiant : MonoBehaviour {
-	public Transform hip;
 	Animator anim;
+	
+	public AudioSource[] aSources;
+	AudioSource audio1;
+	AudioSource audio2;
 	
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
 		anim.speed = 0.25f;
+		
+		audio1 = aSources[0];
+		audio2 = aSources[1];
 	}
 	
 	// Update is called once per frame
@@ -21,6 +27,7 @@ public class AnimateGiant : MonoBehaviour {
 		   	stateInfo.nameHash == Animator.StringToHash("Legs.RobotRightStepStill")){
 			if(Input.GetKeyDown(KeyCode.Q)){
 				anim.SetBool("LeftStep", true);
+				audio1.Play();
 			}
 			if(Input.GetKeyDown(KeyCode.W)){
 				anim.SetBool("RightStep", true);
