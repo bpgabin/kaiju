82d0eab9ed4ce1bd9af53e0b5ef1ba33e0c5740c
bpgabin <bpgabin@gmail.com>
2013-10-24 00:10:09 -0700

    Final prep for Second Playtest
    Updated both versions of tokyo with correct intersections and pagodas.
    Also implemented simple attacking robot AI that moves towards the player
    and ends the game on contact.

---
+++ Assets/Scripts/AttackingRobot.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackingRobot : MonoBehaviour {
+	
+	public Transform monster;
+	public float rotationSpeed = 2.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		Vector3 direction = (monster.position - transform.position).normalized;
+		Quaternion lookRotation = Quaternion.LookRotation(direction);
+		transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+	}
+	
+	void OnCollisionEnter(Collision other){
+		if(other.gameObject.tag == "Player"){
+			GameObject menuObject = GameObject.FindGameObjectWithTag("Menu");
+			MainMenu menu = menuObject.GetComponent<MainMenu>();
+			menu.EndGame();
+		}
+	}
+}
